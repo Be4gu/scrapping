de3b47fbfbe10bd520e8538af77c09a7d05a5cb8
Cristi <you@example.com>
2023-02-11 17:54:42 +0100

    Prueba con un partido que le queda poco

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import * as cheerio from 'cheerio'
 
-const res = await fetch('https://www.vlr.gg/team/matches/1034/nrg-esports/?group=upcoming')
+const res = await fetch(
+  'https://www.vlr.gg/164485/nom-esports-vs-b8-esports-challengers-league-east-surge-split-1-w6'
+)
 const text = await res.text()
 
 const $ = cheerio.load(text)
-const card = $('a.wf-card')
+const card = $('span.match-header-vs-note')
 console.log($(card).html())
