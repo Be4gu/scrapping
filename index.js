18024c185a41d638a9af19d68f707df53ef666c5
Cristi <you@example.com>
2023-02-13 16:38:15 +0100

    Index comentado para que no se actualice el db

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ import * as cheerio from 'cheerio'
 import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 
-const URLS = {
-  matches: 'https://www.vlr.gg/matches',
-}
-//Comentario para detectar un cambio en el commit
-async function scrape(url) {
-  const res = await fetch(url)
-  const text = await res.text()
-  return cheerio.load(text)
-}
+// const URLS = {
+//   matches: 'https://www.vlr.gg/matches',
+// }
+// //Comentario para detectar un cambio en el commit
+// async function scrape(url) {
+//   const res = await fetch(url)
+//   const text = await res.text()
+//   return cheerio.load(text)
+// }
 
-const $ = await scrape(URLS.matches)
-let times = []
-$('div.ml-eta').each((index, el) => {
-  times.push({ item: $(el).text() })
-})
+// const $ = await scrape(URLS.matches)
+// let times = []
+// $('div.ml-eta').each((index, el) => {
+//   times.push({ item: $(el).text() })
+// })
 
-const filePath = path.join(process.cwd(), './db/times.json')
+// const filePath = path.join(process.cwd(), './db/times.json')
 
-await writeFile(filePath, JSON.stringify(times, null, 2), 'utf-8')
+// await writeFile(filePath, JSON.stringify(times, null, 2), 'utf-8')
 
-console.log(filePath)
+// console.log(filePath)
